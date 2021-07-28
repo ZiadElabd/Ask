de3e84ed9d1b15fad889738e1c5de2d95974c959
Ziad Elabd <ziad.elabd91@gmail.com>
2021-07-28 13:30:18 +0200

    initial

---
+++ back-end/src/main/java/Main/backend/AskBackEndApplication.java
@@ -0,0 +1,13 @@
+package Main.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskBackEndApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/Main/backend/AskBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Main.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
