699ed04d1004c90ede248b9b25534a6d99fd8a33
omarrehan0020 <59891874+omarrehan0020@users.noreply.github.com>
2021-09-05 19:59:53 +0200

    navbar in progress

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,6 +1,46 @@
 <template>
   <div id="app">
-    <router-view/>
+    
+    <div class="navbar ">
+
+   <div class="container">
+      <div>
+      <b-navbar-brand href="#">ASKfm</b-navbar-brand>
+    </div>
+    <div>
+      <b-nav>
+        <b-nav-item active> <font-awesome-icon icon="home" /></b-nav-item>
+        <b-nav-item><font-awesome-icon icon="question-circle" /></b-nav-item>
+        <b-nav-item><font-awesome-icon icon="user-friends" /></b-nav-item>
+        <b-nav-item ><font-awesome-icon icon="bolt" /></b-nav-item>
+         
+          <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em><font-awesome-icon icon="cog" /></em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+         <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em><font-awesome-icon icon="plus-square" /></em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+          
+         
+      </b-nav>
+    </div>
+
+   </div>
+
+
+    </div>
+  
     
   </div>
 </template>
@@ -16,12 +56,14 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
- margin: auto;
+.navbar
+{
+  display: flex;
+  justify-content: space-between;
+  background-color: black;
+  
 }
+
 </style>
 
 
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import locale from 'element-ui/lib/locale/lang/en'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(ElementUI, { locale });
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/css/main.css'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome,faQuestionCircle,faUserFriends,faBolt,faCog,faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHome,faQuestionCircle,faUserFriends,faBolt,faCog,faPlusSquare )
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
