0bac67f4b99ffc2c3c5fe3fa264629516f3dacf7
omarrehan0020 <59891874+omarrehan0020@users.noreply.github.com>
2021-09-07 02:05:28 +0200

    navbar style

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,57 +1,21 @@
 <template>
   <div id="app">
-    
-    <div class="navbar ">
-
-   <div class="container">
-      <div>
-      <b-navbar-brand href="#">ASKfm</b-navbar-brand>
-    </div>
-    <div>
-      <b-nav>
-        <b-nav-item active> <font-awesome-icon icon="home" /></b-nav-item>
-        <b-nav-item><font-awesome-icon icon="question-circle" /></b-nav-item>
-        <b-nav-item><font-awesome-icon icon="user-friends" /></b-nav-item>
-        <b-nav-item ><font-awesome-icon icon="bolt" /></b-nav-item>
-         
-          <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em><font-awesome-icon icon="cog" /></em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-         <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em><font-awesome-icon icon="plus-square" /></em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-          
-         
-      </b-nav>
-    </div>
-
-   </div>
-
-
-    </div>
+    <Navbar />
+   
   
     
   </div>
 </template>
 
 <script>
+import Navbar from './components/nbar.vue';
 export default {
   name: 'App',
   components: {
+    Navbar,
     
-    
-  }
+
+      }
 }
 </script>
 
---
+++ front-end/src/components/nbar.vue
@@ -0,0 +1,93 @@
+<template>
+     <div class="navbar ">
+
+   <div class="container">
+      <div>
+      <b-navbar-brand href="#">ASKfm</b-navbar-brand>
+    </div>
+    <div>
+      <b-nav>
+        <b-nav-item active> <font-awesome-icon icon="home" /></b-nav-item>
+        <b-nav-item><font-awesome-icon icon="question-circle" /></b-nav-item>
+        <b-nav-item><font-awesome-icon icon="user-friends" /></b-nav-item>
+        <b-nav-item ><font-awesome-icon icon="bolt" /></b-nav-item>
+         
+          <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em><font-awesome-icon icon="cog" /></em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+         <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em><font-awesome-icon icon="plus-square" /></em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+          
+         
+      </b-nav>
+    </div>
+
+   </div>
+
+
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Navbar',
+    }
+</script>
+
+<style scoped>  
+  
+.nav li a
+{
+  color: #898B8C;
+}
+.nav
+{
+  font-size: 130%;
+}
+
+ .navbar-brand 
+{
+  color: #EE1144;
+  font-weight: bolder;
+  font-family:'Lobster', cursive
+}
+.nav li a:hover ,.navbar-brand:hover
+{
+  color: #EE1144;
+}
+.nav li a:hover
+{
+  font-size: 97.5%;
+}
+
+ div > .navbar-brand
+{
+  font-family:'Lobster', cursive
+}
+.nav-link:focus{
+  color: #EE1144;
+  font-size: 97.5%;
+}
+.container
+{
+  width: 60%;
+}
+.nav-link
+{
+  color: #898B8C;
+}
+
+
+ 
+</style>
