1c65a4c8672ece69d33198b354112d95a45b2f6e
Ziad Elabd <ziad.elabd91@gmail.com>
2021-09-10 17:59:03 +0200

    question page

---
+++ front-end/src/components/QuestionItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class= "question-item"  @click="answerQuestion(question.id)">
+    <div class= "user-profile__twoot">
+        <div class= "twoot-item__user">
+            @{{userName}}
+        </div>
+        <div class= "twoot-item__content">
+            {{question.conntent}}
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuestionItem',
+  props: {
+      userName: {
+        type: String,
+        required: true
+      },
+      question: {
+          type: Object,
+          required: true
+      }
+  },
+  methods: {
+    answerQuestion(id){
+      this.$emit('answer' , id);
+    }
+  }
+}
+</script>
+<style scoped>
+.question-item{
+    padding: 20px;
+    background-color: rgb(155, 111, 111);
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: all 0.25s ease;
+}
+.question-item:hover {
+    transform: scale(1.05,1.05);
+}
+.twoot-item__user {
+    font-weight: bold;
+}
+</style>
--- front-end/src/views/Questions.vue
+++ front-end/src/views/Questions.vue
@@ -1,15 +1,55 @@
 <template>
   <div class="questions">
-    Questions page
+    <div class= "Questions-wrapper">
+      <QuestionItem 
+        v-for ="question in user.questions" 
+        :key="question.id" 
+        :userName="user.userName" 
+        :question = "question"  
+        @answer="answer(question.id)"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 
+import QuestionItem from "../components/QuestionItem.vue";
+
 export default {
   name: 'Questions',
   components: {
-    
-  }
+    QuestionItem
+  },
+  data() {
+    return {
+      user: {
+        id: 1,
+        userName: 'ziadElabd',
+        firstName: 'ziad',
+        secondName: 'elabd',
+        email: 'ziad.elabd91@gmail.com',
+        questions: [
+          { id: 1 , conntent: "ziad is my Name" },
+          { id: 2 , conntent: "osama is my brother" }
+        ]
+      }
+    }
+  },
+  methods: {
+    answer(id){
+      console.log(`Favourite tweet #${id}`);
+    }
+  },
 }
 </script>
+
+<style  scoped>
+
+.Questions-wrapper{
+  display: grid;
+  grid-gap: 10px;
+  padding: 50px 50px 50px 50px;
+}
+
+</style>
